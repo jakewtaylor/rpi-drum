29fcf7ba5d89f54a0e2ef28e8ea40eac5ce7cae4
Jake Taylor <me@jaketaylor.co>
2021-03-05 18:17:17 +0000

    adjustments

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const mpr121 = new MPR121(0x5A, 1);
 
 // listen for touch events
 mpr121.on('touch', (pin) => {
-    console.log(`pin ${pin} touched`)
+    const { name, sound } = sounds[pin] || {};
 
-    if (sounds[pin]) {
-        sounds[pin].play();
+    if (name && sound) {
+        console.log(`${sound}!`);
+        sound.play();
     }
 });
 
--- sounds.js
+++ sounds.js
@@ -2,7 +2,10 @@ const Sound = require('node-aplay');
 
 const soundNames = ['kick', 'snare', 'closed', 'open', 'cymbal', 'clap'];
 
-module.exports = soundNames.map(name => new Sound(`./samples/${name}.wav`));
+module.exports = soundNames.map(name => ({ 
+    name,
+    sound: new Sound(`./samples/${name}.wav`),
+}));
 
 // module.exports = soundNames.reduce((acc, name) => ({
 //     ...acc,
